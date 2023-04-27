4e9af9397fe0c0383b0f5bce8cdf0d7feadc46cd
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-27 19:39:25 +0600

    all package installed and react router prmary setup completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkzinOvFZTQExGi83qd15diNUOq9wkSfU",
+  authDomain: "auth-firebase-context-ta-fb036.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-fb036",
+  storageBucket: "auth-firebase-context-ta-fb036.appspot.com",
+  messagingSenderId: "682669692957",
+  appId: "1:682669692957:web:586c49548ad6ba70b90ef4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
