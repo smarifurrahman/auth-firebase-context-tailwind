35823c9f1d4205c4c9d8c80a7c9c7fc07e881a70
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-30 21:18:56 +0600

    onAuthStateChanged implemented

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                console.log('Sign Out Successful');
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
@@ -9,6 +22,15 @@ const Header = () => {
                 <Link to="/" className="btn btn-ghost normal-case text-lg">Home</Link>
                 <Link to="/login" className="btn btn-ghost normal-case text-lg">Login</Link>
                 <Link to="/register" className="btn btn-ghost normal-case text-lg">Register</Link>
+                {
+                    user ?
+                        <>
+                            <span>{user.email}</span>
+                            <button onClick={handleLogOut} className="btn btn-xs">Sign Out</button>
+                        </> :
+                        <Link to="/login">Sign In</Link>
+
+                }
             </div>
         </div>
     );
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,25 +1,43 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
-const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-}
-
-const signIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-}
-
 const AuthProviders = ({ children }) => {
     const [user, setUser] = useState(null);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    // observe auth state change 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('auth state changed', currentUser);
+            setUser(currentUser);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
         user,
         createUser,
         signIn,
+        logOut,
     }
 
     return (
